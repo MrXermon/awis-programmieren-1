fb55579dada27601c9c629cb3263db3d179b6781
Jan Gilla <xermon@InterBook.local>
2016-09-16 20:58:18 +0200

    Redundanz (#1) entfernt, dadurch allerdings keine statische Funktion mehr.

--- GillaAufg2/src/mypack/Collatz.java
+++ GillaAufg2/src/mypack/Collatz.java
@@ -10,19 +10,44 @@ public class Collatz {
 		double maxLaenge = 0, maxLaengeBei = 0, maxElemente = 0, maxElementeBei = 0, maxRelGroesse = 0,
 				maxRelGroesseBei = 0;
 
+		double n, aktuellElemente, aktuellHoechstes, aktuellRelGroesse;
+		boolean weiter = true;
+
 		DecimalFormat dezFormat = new DecimalFormat("#0.00");
 
 		for (int i = 1; i <= STARTWERT; i++) {
-			if (Collatz.collatz_elemente(i) > maxLaenge) {
-				maxLaenge = Collatz.collatz_elemente(i);
+			weiter = true;
+			n = i;
+			aktuellElemente = 0;
+			aktuellHoechstes = i;
+			aktuellRelGroesse = 0;
+			do {
+				if (n % 2.0 == 0) {
+					n = n / 2.0;
+				} else {
+					n = (3 * n) + 1;
+				}
+				if ((int) n == 1) {
+					aktuellElemente++;
+					weiter = false;
+				} else {
+					aktuellElemente++;
+				}
+				if (n > aktuellHoechstes)
+					aktuellHoechstes = n;
+			} while (weiter);
+			aktuellRelGroesse = aktuellHoechstes / i;
+
+			if (aktuellElemente > maxLaenge) {
+				maxLaenge = aktuellElemente;
 				maxLaengeBei = i;
 			}
-			if ((Collatz.collatz_maxElement(i) - i) > maxElemente) {
-				maxElemente = Collatz.collatz_maxElement(i) - i;
+			if ((aktuellHoechstes - i) > maxElemente) {
+				maxElemente = aktuellHoechstes - i;
 				maxElementeBei = i;
 			}
-			if (Collatz.collatz_relgroesse(i) > maxRelGroesse) {
-				maxRelGroesse = Collatz.collatz_relgroesse(i);
+			if (aktuellRelGroesse > maxRelGroesse) {
+				maxRelGroesse = aktuellRelGroesse;
 				maxRelGroesseBei = i;
 			}
 		}
@@ -32,71 +57,6 @@ public class Collatz {
 				"Maximales Element: " + (int) (maxElemente + maxElementeBei) + " bei n = " + (int) maxElementeBei);
 		System.out.println(
 				"Maximale relative Groesse: " + dezFormat.format(maxRelGroesse) + " bei n = " + (int) maxRelGroesseBei);
-	}
-
-	public static void collatz(double n, boolean ausgabe) {
-		boolean go = true;
-		while (go) {
-			if (ausgabe)
-				System.out.print((int) n);
-			if (n % 2.0 == 0) {
-				n = n / 2.0;
-			} else {
-				n = (3 * n) + 1;
-			}
-			if (n == 1) {
-				if (ausgabe)
-					System.out.print(" -> 1");
-				go = false;
-			} else {
-				if (ausgabe)
-					System.out.print(" -> ");
-			}
-		}
-	}
-
-	public static int collatz_elemente(double n) {
-		// Kettenlaenge
-		boolean go = true;
-		int zaehler = 0;
-		while (go) {
-			if (n % 2.0 == 0) {
-				n = n / 2.0;
-			} else {
-				n = (3 * n) + 1;
-			}
-			if ((int) n == 1) {
-				zaehler++;
-				go = false;
-			} else {
-				zaehler++;
-			}
-		}
-		return zaehler;
-	}
-
-	public static int collatz_maxElement(double n) {
-		// Hoechstes Element
-		boolean go = true;
-		double maximales = n;
-		while (go) {
-			if (n % 2.0 == 0) {
-				n = n / 2.0;
-			} else {
-				n = (3 * n) + 1;
-			}
-			if ((int) n == 1) {
-				go = false;
-			}
-			if (n > maximales)
-				maximales = n;
-		}
-		return (int) maximales;
-	}
-
-	public static double collatz_relgroesse(double n) {
-		// rel. Groesse
-		return collatz_maxElement(n) / n;
 	}
 
 }
